af90dc134949ee0264500ea1ad67713fa318337d
Emanmohsenmoraad <150667574+Emanmohsenmoraad@users.noreply.github.com>
2023-11-27 23:27:32 +0200

    Add files via upload

---
+++ task6/task6.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task6',
+  templateUrl: './task6.component.html',
+  styleUrls: ['./task6.component.css']
+})
+export class Task6Component {
+  userName: string = 'Eman Mohsen ';
+  processInput(inputValue: string): void {
+
+    console.log('Input value:', inputValue);
+
+  }
+ /*videoId = 'https://www.youtube.com/watch?v=zsia54kAOF8';*/
+
+  isButtonActive: boolean=false;
+  color: string = '#FF0000'; // Red color as an initial value
+
+  
+  updateColor(newColor: string) {
+    this.color = newColor;
+  }
+  resetColor() {
+    this.color = '#FF0000';
+}
+
+
+}
